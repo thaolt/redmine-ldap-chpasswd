5ed3ea3d17b666a94841b1cdf528713becbf833c
Thao, Le Thach <thaolt@songphi.com>
2011-11-15 20:21:31 +0000

    initialized redmine plugin project

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_ldap_chpasswd do
+  name 'Redmine Ldap Chpasswd plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
